a20687c4c878079e7d6cf05ea61d5b561d660c6f
Teo1112 <t.puiu@yahoo.com>
2024-05-20 18:43:09 +0300

    Ex3 - sir carti => afisare carti citite/necitite

---
+++ Ex3/ex3.js
@@ -0,0 +1,22 @@
+//sir de obiecte
+const carti = [
+    { titlu: "Hobbitul", autor: "J.R.R. Tolkien", esteCitita: true },
+    { titlu: "1984", autor: "George Orwell", esteCitita: false },
+    { titlu: "Mândrie și prejudecată", autor: "Jane Austen", esteCitita: true },
+    { titlu: "Crimă și pedeapsă", autor: "Fyodor Dostoevsky", esteCitita: false }
+];
+
+function afiseazaCarti(carti) {
+    for (const carte of carti) {
+
+        console.log(`${carte.titlu} de ${carte.autor}`); //afisam titlu si autor
+        
+        if (carte.esteCitita) {
+            console.log(`Ai citit deja "${carte.titlu}" de ${carte.autor}`);
+        } else {
+            console.log(`Trebuie să citești "${carte.titlu}" de ${carte.autor}`);
+        }
+    }
+}
+
+afiseazaCarti(carti);
