2339bdf2ee43feb333a53b5e99d2303ed5d5549b
Teo1112 <147054142+Teo1112@users.noreply.github.com>
2024-06-12 12:17:24 +0300

    Update ex1.js

--- Ex1/ex1.js
+++ Ex1/ex1.js
@@ -1,11 +1,18 @@
-const reteta = {
-    titlu: "Tiramisu",
-    portii: 6,
-    ingrediente: ["piscoturi", "mascarpone", "oua", "zahar", "cafea"]
-};
+function creeazaReteta(titlu, portii, ingrediente) {
+    const reteta = {
+        titlu: titlu,
+        portii: portii,
+        ingrediente: ingrediente
+    };
 
-console.log("Titlu: " + reteta.titlu);
-console.log("Porții: " + reteta.portii);
-console.log("Ingrediente: " + reteta.ingrediente.join(", "));
+    console.log("Titlu: " + reteta.titlu);
+    console.log("Porții: " + reteta.portii);
+    console.log("Ingrediente: " + reteta.ingrediente.join(", "));
+    
+    return reteta;
+}
 
-//Metoda join în JavaScript este utilizată pentru a combina toate elementele unui array într-un singur șir de caractere, separându-le cu un separator specificat (virgula).
+const titlu = "Tiramisu";
+const portii = 6;
+const ingrediente = ["piscoturi", "mascarpone", "oua", "zahar", "cafea"];
+const retetaTiramisu = creeazaReteta(titlu, portii, ingrediente);
